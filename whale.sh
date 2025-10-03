9f0f373b085b8ffc1821b15ea04087d71e6fb2bd
Fratzeska Petraki <fratzeska@aristotle5.it.auth.gr>
2025-10-03 12:12:08 +0300

    I pass the .sh files

---
+++ whale.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=whale_test
+#SBATCH --ntasks=1
+#SBATCH --mem=64G
+#SBATCH --gres=gpu:1
+#SBATCH --partition=ampere
+#SBATCH --time=2:00:00
+
+source ~/myenv/bin/activate
+python3 ~/E/Whale_Project/whale.py
