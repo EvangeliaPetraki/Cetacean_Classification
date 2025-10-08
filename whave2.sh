f7632ca81cf7e0bbf55d165a80049713c0a594f8
Fratzeska Petraki <fratzeska@aristotle6.it.auth.gr>
2025-10-08 11:02:11 +0300

    changed name of .sh file

--- whave2.sh
+++ whave2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=whale_test
+#SBATCH --job-name=whale_No_aug
 #SBATCH --ntasks=1
 #SBATCH --mem=64G
 #SBATCH --gres=gpu:1
 #SBATCH --partition=ampere
-#SBATCH --time=1:00:00
+#SBATCH --time=2:00:00
 
 
 source ~/myenv/bin/activate
 
-python3 ~/E/Cetacean_Classification/whale_original.py
+python3 ~/E/Cetacean_Classification/whale2.py
