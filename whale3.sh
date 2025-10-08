ba9312fdee6508949a3a67b1435ba217a4c363fc
Evangelia Petraki <petrakievaggelia@gmail.com>
2025-10-08 11:08:16 +0300

    Create whale3.sh

---
+++ whale3.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=whale_No_aug
+#SBATCH --ntasks=1
+#SBATCH --mem=64G
+#SBATCH --gres=gpu:1
+#SBATCH --partition=ampere
+#SBATCH --time=2:00:00
+
+
+source ~/myenv/bin/activate
+
+python3 ~/E/Cetacean_Classification/whale3.py
